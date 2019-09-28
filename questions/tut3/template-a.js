2d41a47cddac66d91930e1870dd0f98f194fb57b
Jun Zheng <me@jackzh.com>
2019-09-28 11:45:34 -0400

    feat: Add ability to have multiple inputs

--- questions/tut3/template-a.js
+++ questions/tut3/template-a.js
@@ -2,19 +2,43 @@ const app = new Vue({
     el: '#app',
     data: {
         questionConfig: QUESTION_CONFIGURATION,
+        currentSection: 0,
         studentAnswer: [],
+        failedAttempts: [],
         currentLine: "",
         hasError: false,
     },
     methods: {
         submitCurrentLine() {
-            const correctLine = this.questionConfig.answer[this.studentAnswer.length];
+            if(!this.studentAnswer[this.currentSection]) {
+                this.studentAnswer[this.currentSection] = [];
+            }
+            const correctLine = this.questionConfig.subsections[this.currentSection].answer[this.getAnswer(this.currentSection).length];
             if (correctLine !== this.currentLine) {
                 this.hasError = true;
+                if(!this.failedAttempts[this.currentSection]) {
+                    this.failedAttempts[this.currentSection] = 1;
+                } else {
+                    this.failedAttempts[this.currentSection]++;
+                }
             } else {
                 this.hasError = false;
-                this.studentAnswer.push(this.currentLine);
+                this.studentAnswer[this.currentSection].push(this.currentLine);
                 this.currentLine = "";
+            }
+        },
+        getFailedAttempts(section) {
+            if(!this.failedAttempts[this.currentSection]) {
+                return 0;
+            } else {
+                return this.failedAttempts[this.currentSection];
+            }
+        },
+        getAnswer(section) {
+            if(this.studentAnswer[section]) {
+                return this.studentAnswer[section];
+            } else {
+                return [];
             }
         }
     }
