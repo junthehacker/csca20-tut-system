40b09c35dfeaf4dbf4e9cc345e4cb6900f19eaa2
Jun Zheng <me@jackzh.com>
2019-10-06 11:12:14 -0400

    feat: Add CSCA20 lab4

---
+++ src/resources/TutFourQuestions.js
@@ -0,0 +1,47 @@
+module.exports = [
+    {
+        "title": "Question 1",
+        "path": "tut4/question1",
+    },
+    {
+        "title": "Question 2",
+        "path": "tut4/question2",
+    },
+    {
+        "title": "Question 3",
+        "path": "tut4/question3",
+    },
+    {
+        "title": "Question 4",
+        "path": "tut4/question4",
+    },
+    {
+        "title": "Question 5",
+        "path": "tut4/question5",
+    },
+    {
+        "title": "Question 6",
+        "path": "tut4/question6",
+        "noAnswer": true,
+    },
+    {
+        "title": "Question 7",
+        "path": "tut4/question7",
+        "noAnswer": true,
+    },
+    {
+        "title": "Question 8",
+        "path": "tut4/question8",
+        "noAnswer": true,
+    },
+    {
+        "title": "Question 9",
+        "path": "tut4/question9",
+        "noAnswer": true,
+    },
+    {
+        "title": "Question 10",
+        "path": "tut4/question10",
+        "noAnswer": true,
+    },
+];
